87ba0d4a8dce2512c2da8d83b76b16e10003180c
Barczii <50383406+Bartlok477@users.noreply.github.com>
2019-12-29 17:12:47 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,111 @@
+
+
+
+function copy() {
+    
+    
+    var copyText = document.querySelector("#p1");
+    copyText.select();
+    document.execCommand("copy",false,null);
+    
+    
+}
+
+
+
+
+(function() {
+    
+    document.addEventListener('DOMContentLoaded',function() {
+        document.getElementById("button_1").addEventListener("click", copy);
+    })
+
+
+    const formInstance = document.querySelector('#generator');
+    const output = document.querySelector('#password');
+    
+    const start_value=8;
+    document.getElementById("passlength").setAttribute('value',start_value);
+
+
+
+    const chars = {
+        letters: 'qwertyuiopasdfghjklzxcvbn',
+        capitals: 'QWERTYUIOPASDFGHJKLZXCVBNM',
+        digits: '1234567890',
+        special: '`~!@#$%^&*()-_=+[{]}\\|;:\'\",<.>/?'
+    }
+
+    const helper = {
+        getRandomInt: function(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min;
+        }
+    }
+
+    const form = {
+        getJSON: (form) => {
+            const formData = new FormData(form),
+                data = {};
+
+            for (let input of formData.entries()) {
+                data[input[0]] = input[1];
+            }
+            return data;
+        },
+        isEmpty: (data) => {
+            if (data['passlength'] === '') {
+                return true;
+            }
+            return false;
+        },
+        hasChars: (data, groupname)=>{
+            return groupname in data;
+        }
+    }
+
+    const generator = {
+        generateCharSet:(hasCaps, hasDigits, hasSpecial, chars)=>{
+            let set = chars.letters;
+            if(hasCaps) set += chars.capitals;
+            if(hasDigits) set += chars.digits + chars.digits;
+            if(hasSpecial) set += chars.special;
+            return set;
+        },
+        generatePassword:(length, charset)=>{
+            let password = '';
+            for(let i =0;i<length;i++) {
+                password += charset[helper.getRandomInt(0, charset.length)]
+            }
+            return password;
+        }
+    }
+
+    formInstance.addEventListener('submit', (e) => {
+        const data = form.getJSON(formInstance);
+        if(!form.isEmpty(data)){
+            const charset = generator.generateCharSet(
+                form.hasChars(data,'capitals'),
+                form.hasChars(data,'digits'),
+                form.hasChars(data, 'special'),
+                chars);
+            const password = generator.generatePassword(data.passlength, charset);
+            
+            var miejsce=document.getElementById("p1");
+
+            miejsce.setAttribute('value',password);
+            
+           
+
+        } else {
+            alert('Uzupełnij długość hasła');
+        }
+        e.preventDefault();
+    })
+
+  
+
+
+
+})();
